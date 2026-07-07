14668a0c74e2a9d061f7133bcc27ce69d4b40212
G-Works <fabian.graf@live.de>
2026-07-07 10:24:19 +0200

    Add domain redirects to gworks.li

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,54 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "gworks.ch",
+          },
+        ],
+        destination: "https://www.gworks.li/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.gworks.ch",
+          },
+        ],
+        destination: "https://www.gworks.li/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "gworks.at",
+          },
+        ],
+        destination: "https://www.gworks.li/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.gworks.at",
+          },
+        ],
+        destination: "https://www.gworks.li/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
